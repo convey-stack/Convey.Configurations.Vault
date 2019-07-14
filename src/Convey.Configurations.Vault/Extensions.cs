5b9c89277a38eec57bdbbbe9e3141650a972c832
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-07-14 16:54:42 +0200

    UseVault extension

--- src/Convey.Configurations.Vault/Extensions.cs
+++ src/Convey.Configurations.Vault/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Convey.Configurations.Vault.Stores;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,47 +14,41 @@ namespace Convey.Configurations.Vault
         private const string SectionName = "vault";
         private const string RegistryName = "configurations.vault";
 
-        public static IConveyBuilder AddVault(this IConveyBuilder builder, string sectionName = SectionName)
-        {
-            var options = builder.GetOptions<VaultOptions>(sectionName);
-            builder.Services.AddSingleton(options);
-            if (!options.Enabled || !builder.TryRegister(RegistryName))
-            {
-                return builder;
-            }
+        public static IWebHostBuilder UseVault(this IWebHostBuilder builder, string key = null)
+            => builder.ConfigureServices(services =>
+                {
+                    IConfiguration configuration;
+                    using (var serviceProvider = services.BuildServiceProvider())
+                    {
+                        configuration = serviceProvider.GetService<IConfiguration>();
+                    }
 
-            builder.Services.AddTransient<IVaultStore, VaultStore>();
+                    var options = configuration.GetOptions<VaultOptions>("vault");
+                    if (!options.Enabled)
+                    {
+                        return;
+                    }
 
-            return builder;
-        }
+                    services.AddSingleton(options);
+                    services.AddTransient<IVaultStore, VaultStore>();
+                })
+                .ConfigureAppConfiguration((ctx, cfg) =>
+                {
+                    var options = cfg.Build().GetOptions<VaultOptions>("vault");
+                    var enabled = options.Enabled;
+                    var vaultEnabled = Environment.GetEnvironmentVariable("VAULT_ENABLED")?.ToLowerInvariant();
+                    if (!string.IsNullOrWhiteSpace(vaultEnabled))
+                    {
+                        enabled = vaultEnabled == "true" || vaultEnabled == "1";
+                    }
 
-        public static IApplicationBuilder UseVault(this IApplicationBuilder app, string key = null)
-        {
-            VaultOptions options;
-            IConfigurationBuilder configurationBuilder;
-            using (var scope = app.ApplicationServices.CreateScope())
-            {
-                configurationBuilder = scope.ServiceProvider.GetService<IConfigurationBuilder>();
-                options = scope.ServiceProvider.GetService<VaultOptions>();
-            }
+                    if (enabled)
+                    {
+                        cfg.AddVault(options, key);
+                    }
+                });
 
-            var enabled = options.Enabled;
-            var vaultEnabled = Environment.GetEnvironmentVariable("VAULT_ENABLED")?.ToLowerInvariant();
-            if (!string.IsNullOrWhiteSpace(vaultEnabled))
-            {
-                enabled = vaultEnabled == "true" || vaultEnabled == "1";
-            }
-
-            if (enabled)
-            {
-                configurationBuilder.AddVault(options, key);
-            }
-
-            return app;
-        }
-
-        private static void AddVault(this IConfigurationBuilder builder,
-            VaultOptions options, string key)
+        private static void AddVault(this IConfigurationBuilder builder, VaultOptions options, string key)
         {
             var client = new VaultStore(options);
             var secret = string.IsNullOrWhiteSpace(key)
